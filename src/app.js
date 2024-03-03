7e0f4e73ef99171ff30c8b997799baf4640ff24e
gataitai <mpattipeilohy1@gmail.com>
2024-03-03 01:55:56 +0100

    yas3

--- src/app.js
+++ src/app.js
@@ -13,6 +13,34 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Route to capture and send a single frame from raspivid
+app.get('/photo', (req, res) => {
+    // Start raspistill to capture a single frame
+    const raspistill = spawn('raspistill', [
+        '-w', '640',    // Width
+        '-h', '480',    // Height
+        '-o', '-'       // Output to stdout
+    ]);
+
+    // Buffer to store captured image data
+    let imageData = Buffer.alloc(0);
+
+    // Concatenate data chunks from raspistill
+    raspistill.stdout.on('data', (chunk) => {
+        imageData = Buffer.concat([imageData, chunk]);
+    });
+
+    // Send captured image data when raspistill exits
+    raspistill.on('close', (code) => {
+        if (code === 0) {
+            res.set('Content-Type', 'image/jpeg');
+            res.send(imageData);
+        } else {
+            res.status(500).send('Error capturing photo');
+        }
+    });
+});
+
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
