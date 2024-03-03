9400e5294333fba00637c9deeffa6e0744ec9a5b
gataitai <mpattipeilohy1@gmail.com>
2024-03-03 02:28:01 +0100

    yas3

--- src/app.js
+++ src/app.js
@@ -6,17 +6,11 @@ const app = express();
 
 // Function to capture a single frame and send it as JPEG
 function capturePhoto(callback) {
-    // Create a writable stream to capture the photo
-    const photoStream = fs.createWriteStream('photo.jpg');
-
     // Use ffmpeg to capture a single frame from the video stream
     const ffmpegProcess = spawn('ffmpeg', ['-i', 'pipe:0', '-frames:v', '1', '-f', 'image2', 'pipe:1']);
 
     // Pipe the video stream into ffmpeg
     videoStream.pipe(ffmpegProcess.stdin);
-
-    // Pipe the output of ffmpeg into the photo stream
-    ffmpegProcess.stdout.pipe(photoStream);
 
     // When ffmpeg finishes, call the callback function
     ffmpegProcess.on('exit', () => {
