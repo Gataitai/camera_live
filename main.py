abde6054ec057db172bd65a48f3f96720dcf2c1c
Mathijs Pattipeilohy <mpattipeilohy1@gmail.com>
2025-07-21 21:57:28 +0200

    added picamera2 script

---
+++ main.py
@@ -0,0 +1,19 @@
+import io
+
+from picamera2 import PiCamera
+from fastapi import FastAPI
+from fastapi.responses import Response
+
+app = FastAPI()
+
+@app.get("/image")
+def get_image():
+    picam2 = PiCamera2()
+    capture_config = picam2.create_still_configuration(main={"size": (640, 480)})
+    picam2.capture_config(capture_config)
+    data = io.BytesIO()
+    picam2.start()
+    picam2.capture_file(data, format="jpeg")
+    picam2.stop()
+    picam2.close()
+    return Response(content=data.getvalue(), media_type="image/jpeg")
